c87d40f1c6712eca7a4966fd42f56c7102c6f5c1
Yeonho Lee <tomatoyeonho@naver.com>
2018-02-20 22:04:04 +0900

    업데이트 0.1.5 (Update 0.1.5)
     - wsgi2.py 코드 깔끔하게 수정
     - Edited wsgi2.py code as clean

--- wsgi2.py
+++ wsgi2.py
@@ -8,38 +8,23 @@ def application(environ, start_response):
     currentTime = datetime.now().strftime('%H:%M')
     currentTime_front = int(currentTime[0:2])
     L = []
+    ymd = time.strftime('%Y%m%d')
     if currentTime_front > 02:
-        
-        ymd = time.strftime('%Y/%m/%d')
-        L.append((ymd.replace('/',''), '0200'))
-        if currentTime_front > 05:
-            
-            ymd = time.strftime('%Y/%m/%d')
-            L.append((ymd.replace('/',''), '0500'))
-            if currentTime_front > 8:
-                
-                ymd = time.strftime('%Y/%m/%d')
-                L.append((ymd.replace('/',''), '0800'))
-                if currentTime_front > 11:
-                    
-                    ymd = time.strftime('%Y/%m/%d')
-                    L.append((ymd.replace('/',''), '1100'))
-                    if currentTime_front > 14:
-                        
-                        ymd = time.strftime('%Y/%m/%d')
-                        L.append((ymd.replace('/',''), '1400'))
-                        if currentTime_front > 17:
-                            
-                            ymd = time.strftime('%Y/%m/%d')
-                            L.append((ymd.replace('/',''), '1700'))
-                            if currentTime_front > 20:
-                                
-                                ymd = time.strftime('%Y/%m/%d')
-                                L.append((ymd.replace('/',''), '2000'))
-                                if currentTime_front > 23:
-                                    
-                                    ymd = time.strftime('%Y/%m/%d')
-                                    L.append((ymd.replace('/',''), '2300'))
+        L.append((ymd, '0200'))
+    if currentTime_front > 05:
+        L.append((ymd, '0500'))
+    if currentTime_front > 8:
+        L.append((ymd, '0800'))
+    if currentTime_front > 11:
+        L.append((ymd, '1100'))
+    if currentTime_front > 14:
+        L.append((ymd, '1400'))
+    if currentTime_front > 17:
+        L.append((ymd, '1700'))
+    if currentTime_front > 20:
+        L.append((ymd, '2000'))
+    if currentTime_front > 23:
+        L.append((ymd, '2300'))
     response_body = json.dumps(L)
     response_header = [('Content-Type','text/json'),('Content-Length', str(len(response_body)))]
     start_response('200 OK', response_header)
